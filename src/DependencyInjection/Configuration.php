b5aaa92f5e968f122ef58caa96e9f7e9a9dd9ed9
Tim Brouckaert <tim@sweetsally.be>
2021-05-18 21:36:06 +0200

    fix and complete config

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -14,9 +14,14 @@ class Configuration implements ConfigurationInterface
         $rootNode = $treeBuilder->getRootNode();
         $rootNode
             ->children()
-            ->scalarNode('paths')->end()
-            ->scalarNode('form_themes')->end()
-            ->end()
-        ;
+                ->scalarNode('paths')
+                    ->defaultValue(['%kernel.project_dir%/vendor/sweet-sally-be/forms-bundle/src/Form/View' => 'FormsBundle'])
+                    ->info('Path to the view')
+                    ->end()
+                ->scalarNode('form_themes')
+                    ->defaultValue(['@FormsBundle/fields.html.twig'])
+                    ->end()
+            ->end();
+        return $treeBuilder;
     }
 }
--- src/DependencyInjection/FormsExtension.php
+++ src/DependencyInjection/FormsExtension.php
@@ -6,6 +6,7 @@ namespace SweetSallyBe\FormsBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\FileLoader;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
@@ -15,6 +16,10 @@ class FormsExtension extends Extension
     {
         $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.xml');
+
+        $configuration = $this->getConfiguration($configs, $container);
+        $config = $this->processConfiguration($configuration, $configs);
+        $definition = $container->getDefinition('sweet_sally_be.forms_bundle.forms_bundle');
     }
 
 }
